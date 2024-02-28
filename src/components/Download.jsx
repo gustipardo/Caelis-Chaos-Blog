48bb8a70834302ba9462d635b2155e8aaa1e59bf
gustipardo <gusm200@gmail.com>
2024-02-28 12:52:56 -0300

    ant desing icons throw error

---
+++ src/components/Download.jsx
@@ -0,0 +1,15 @@
+
+import { Button } from "antd"
+
+
+const Download = () => {
+
+    return(
+        <>
+         <Button style={{ background: 'green', borderColor: 'green' }} type="primary">Download</Button>
+
+        </>
+    )
+}
+
+export default Download
