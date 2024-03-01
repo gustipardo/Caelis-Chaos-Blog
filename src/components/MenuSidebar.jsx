e7c42597cdbb5db2d5ad610359121ab50769bcdc
gustipardo <gusm2003@gmail.com>
2024-03-01 17:08:35 -0300

    MenuSidebar transform into topbar on Desktop

--- src/components/MenuSidebar.jsx
+++ src/components/MenuSidebar.jsx
@@ -52,7 +52,7 @@ const MenuSidebar = () => {
   const handleItemClick = (item) => {
     console.log('Item clickeado:', item);
     currentPage.current = item;
-    history.push(item);
+  history.push(item);
   };
   const items = [
     {
@@ -84,7 +84,6 @@ const MenuSidebar = () => {
       </button>
 
         <div className={`sidebar ${isFold ? 'disappear' : 'appear'}`}>
-          <a href='updates'>updates</a>
                     <Menu defaultSelectedKeys={currentPage.current} className='sidebar-menu' mode="inline" onClick={({ key }) => handleItemClick(key)} items={items}/>
         </div>
 
