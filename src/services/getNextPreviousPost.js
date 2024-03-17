6b4ce2046abd99ca5852832f1140e734956556aa
gustipardo <gusm2003@gmail.com>
2024-03-17 11:50:26 -0300

    Posts order navigation fixed

--- src/services/getNextPreviousPost.js
+++ src/services/getNextPreviousPost.js
@@ -1,15 +1,12 @@
 
 
 const getNextPreviousPost = ({isNext=true, postname, sortedPosts} ) => {
-
-    const currentPostinfo = sortedPosts.find(objeto => objeto.url === `/posts/${postname}`);
-    const currentPostDate = currentPostinfo.frontmatter.date
     
     // Encuentra el índice del post actual en el array de posts ordenados
     const currentIndex = sortedPosts.findIndex(objeto => objeto.url === `/posts/${postname}`);
 
     // Calcula el índice del post siguiente o anterior según el valor de isNext
-    const nextIndex = isNext ? currentIndex + 1 : currentIndex - 1;
+    const nextIndex = isNext ? currentIndex - 1 : currentIndex + 1; // IsNext tiene - 1 porque en el array sortedPosts esta ordenado de menor a mayor
 
     // Devuelve el post correspondiente al índice calculado, o null si está fuera de los límites del array
     const nextPreviousPost = sortedPosts[nextIndex];
