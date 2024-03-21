38fd4dfad39558b4d67cc099a3bab63e955ca180
gustipardo <gusm200@gmail.com>
2024-03-20 23:16:48 -0300

    trailer added to anchor

--- src/components/MenuSidebar.jsx
+++ src/components/MenuSidebar.jsx
@@ -79,7 +79,7 @@ const MenuSidebar = () => {
     getItem(<a href='/'>Home</a>, '/', Home),
     getItem(<a href='/updates'>Blog</a>, '/updates', Blog),
     getItem(<a href='/forum'>Forum</a>, '/forum', Forum),
-    getItem(<a href='/trailer'>Trailer</a>, '/trailer', Youtube)
+    getItem(<a href='https://www.youtube.com/embed/3wMDPq6bHUw'>Trailer</a>, '/trailer', Youtube)
   ];
   return (
     <div className='container'>
