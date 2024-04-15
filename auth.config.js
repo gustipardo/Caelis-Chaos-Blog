d77a387d989858bedf715066742569bb78eca518
gustipardo <gusm2003@gmail.com>
2024-04-15 02:28:44 -0300

    Google provider added

--- auth.config.js
+++ auth.config.js
@@ -1,5 +1,6 @@
 import GitHub from '@auth/core/providers/github'
 import Twitch from '@auth/core/providers/twitch'
+import Google from '@auth/core/providers/google'
 import { defineConfig } from 'auth-astro'
 
 export default defineConfig({
@@ -11,6 +12,10 @@ export default defineConfig({
 		GitHub({
 			clientId: import.meta.env.GITHUB_CLIENT_ID,
 			clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
+		}),
+		Google({
+			clientId: import.meta.env.GOOGLE_CLIENT_ID,
+			clientSecret: import.meta.env.GOOGLE_CLIENT_SECRET,
 		})
 	],
 })
