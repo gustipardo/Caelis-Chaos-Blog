8b8d5283554b075d0fc4c59473df0bacdfaba109
gustipardo <gusm2003@gmail.com>
2024-04-15 00:15:46 -0300

    Astro autg add and vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import auth from "auth-astro";
 
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), react()]
+  integrations: [tailwind(), mdx(), react(), auth()],
+  output: "server",
+  adapter: vercel()
 });
