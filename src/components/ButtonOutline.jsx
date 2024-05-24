dddf9b3dd1a84c1b5ede2dfba3337cc797b97076
gustipardo <gusm2003@gmail.com>
2024-05-23 22:57:02 -0300

    LogIn PopUp when trying to comment without session

--- src/components/ButtonOutline.jsx
+++ src/components/ButtonOutline.jsx
@@ -1,25 +1,27 @@
-import { Button } from "antd"
-import "@/styles/ButtonOutline.css"
-import '@/styles/global.css'
-import getCurrentPost from "@/services/getCurrentPost"
-import getNextPreviousPost from "@/services/getNextPreviousPost.js"
+import { Button } from "antd";
+import "@/styles/ButtonOutline.css";
+import "@/styles/global.css";
+import getCurrentPost from "@/services/getCurrentPost";
+import getNextPreviousPost from "@/services/getNextPreviousPost.js";
 
-export const ButtonOutline = ({children, isNext=true, sortedPosts}) => {
-    const currentPost = getCurrentPost()
-    const nextPreviousPost = getNextPreviousPost({isNext , postname: currentPost, sortedPosts})
-    return(
-    
-        <>
-            {nextPreviousPost && (
-                <a href={nextPreviousPost}>
-                    <Button className="buttonOutline" type="primary" ghost>
-                        {children}
-                    </Button>
-                </a>
-            )}
-        </>
+export const ButtonOutline = ({ children, isNext = true, sortedPosts }) => {
+  const currentPost = getCurrentPost();
+  const nextPreviousPost = getNextPreviousPost({
+    isNext,
+    postname: currentPost,
+    sortedPosts,
+  });
+  return (
+    <>
+      {nextPreviousPost && (
+        <a href={nextPreviousPost}>
+          <Button className="buttonOutline" type="primary" ghost>
+            {children}
+          </Button>
+        </a>
+      )}
+    </>
+  );
+};
 
-    )
-}
-
-export default ButtonOutline
+export default ButtonOutline;
